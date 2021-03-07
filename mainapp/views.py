64ae0c12ef3e08e5bdd88d946c8c2ffe37b9d8ca
Taras <tarash281@gmail.com>
2021-03-07 17:46:52 +0200

    more info added and design improved

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,12 +13,21 @@ def home_page(request):
 
 
 def test_speed(request):
-    # speed - Mbit/s
-    # ping - ms
     st = speedtest.Speedtest()
     speed_data = {
         'download_speed': "{:.2f}".format(st.download() / 10 ** 6),
         'upload_speed': "{:.2f}".format(st.upload() / 10 ** 6),
         'ping': st.results.ping,
+        'country': st.results.client['country'],
+        'ip_address': st.results.client['ip'],
+        'provider': st.results.client['isp'],
     }
+    # speed_data = {
+    #     'download_speed': 100.00,
+    #     'upload_speed': 100.00,
+    #     'ping': 10.00,
+    #     'country': st.results.client['country'],
+    #     'ip_address': st.results.client['ip'],
+    #     'provider': st.results.client['isp'],
+    # }
     return render(request, 'mainapp/test.html', speed_data)
